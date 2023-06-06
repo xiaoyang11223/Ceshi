4ab2c6052b646645f0ebaf8d8c314224ff4336c2
xiaoyang11223 <102507825+xiaoyang11223@users.noreply.github.com>
2023-06-06 19:59:46 +0800

    Update WebAppCeshi.py

--- WebAppCeshi.py
+++ WebAppCeshi.py
@@ -50,7 +50,7 @@ st.sidebar.title("Features")
 # 设置各项特征的输入范围和选项
 Age = st.sidebar.selectbox(label='Age', options=[1,2,3,4,5,6], index=1)#label里面是标签，可以随意更改
 Gender = st.sidebar.selectbox(label='Gender', options=[0, 1], format_func=lambda x: option_name1(x), index=0)
-Smokeing = st.sidebar.selectbox(label='Smoke', options=[0,1], format_func=lambda x: option_name(x), index=0)
+Smokeing11 = st.sidebar.selectbox(label='Smoke', options=[0,1], format_func=lambda x: option_name(x), index=0)
 Drinking = st.sidebar.selectbox(label='Drink', options=[0,1], format_func=lambda x: option_name(x), index=0)
 Diabetes = st.sidebar.selectbox(label='Diabetes', options=[0,1], format_func=lambda x: option_name(x), index=0)
 Heart_disease = st.sidebar.selectbox(label='Heart.disease', options=[0,1], format_func=lambda x: option_name(x), index=0)
@@ -109,7 +109,7 @@ AST = st.sidebar.number_input(label='AST', min_value=0.00,
 
 
 features = {'Age': Age, 'Gender': Gender,
-            'Smokeing': Smokeing, 'Drinking': Drinking,
+            'Smokeing': Smokeing11, 'Drinking': Drinking,
             'Diabetes': Diabetes, 'Heart.disease': Heart_disease,
             'BMI':BMI,
             'WBC': WBC, 'RBC': RBC,
