ae10f07aee32d1442031b552923ffbf3289b8bd0
xiaoyang11223 <102507825+xiaoyang11223@users.noreply.github.com>
2023-06-06 16:07:49 +0800

    Update WebAppCeshi.py

--- WebAppCeshi.py
+++ WebAppCeshi.py
@@ -44,63 +44,64 @@ st.sidebar.title("Features")
 # 设置各项特征的输入范围和选项
 Age = st.sidebar.selectbox(label='Age', options=[1,2,3,4,5,6], index=1)#label里面是标签，可以随意更改
 Gender = st.sidebar.selectbox(label='Gender', options=[0, 1], format_func=lambda x: option_name(x), index=0)
+BMI = st.sidebar.number_input(label='BMI', min_value=15.20,
+                                  max_value=38.90,
+                                  value=15.20,
+                                  step=0.01)
 Smokeing = st.sidebar.selectbox(label='Smokeing', options=[0,1], format_func=lambda x: option_name(x), index=0)
 Drinking = st.sidebar.selectbox(label='Drinking', options=[0,1], format_func=lambda x: option_name(x), index=0)
 Diabetes = st.sidebar.selectbox(label='Diabetes', options=[0,1], format_func=lambda x: option_name(x), index=0)
 Heart_disease = st.sidebar.selectbox(label='Heart.disease', options=[0,1], format_func=lambda x: option_name(x), index=0)
-BMI = st.sidebar.number_input(label='BMI', min_value=15.0,
-                                  max_value=40.0,
-                                  value=15.0,
-                                  step=0.1)
 
-WBC = st.sidebar.number_input(label='WBC', min_value=2.0,
-                                  max_value=28.0,
-                                  value=2.0,
-                                  step=0.1)
-RBC = st.sidebar.number_input(label='RBC', min_value=2.0,
-                                  max_value=7.8,
-                                  value=2.0,
-                                  step=0.1)
 
-PLT = st.sidebar.number_input(label='PLT', min_value=53.0,
-                                  max_value=737.0,
-                                  value=53.0,
-                                  step=2.0)
-HGB = st.sidebar.number_input(label='HGB', min_value=58.0,
-                                  max_value=197.0,
-                                  value=60.0,
-                                  step=1.0)
+WBC = st.sidebar.number_input(label='WBC', min_value=2.00,
+                                  max_value=28.13,
+                                  value=2.00,
+                                  step=0.01)
+RBC = st.sidebar.number_input(label='RBC', min_value=2.07,
+                                  max_value=7.86,
+                                  value=2.07,
+                                  step=0.01)
 
-HCT = st.sidebar.number_input(label='HCT', min_value=17.7,
-                                  max_value=58.5,
-                                  value=18.0,
-                                  step=2.2)
+PLT = st.sidebar.number_input(label='PLT', min_value=53.00,
+                                  max_value=737.00,
+                                  value=53.00,
+                                  step=2.00)
+HGB = st.sidebar.number_input(label='HGB', min_value=58.00,
+                                  max_value=197.00,
+                                  value=60.00,
+                                  step=1.00)
 
-Neu = st.sidebar.number_input(label='Neu', min_value=1.0,
-                                  max_value=25.0,
-                                  value=2.0,
-                                  step=1.0)
-PT = st.sidebar.number_input(label='PT', min_value=8.9,
-                                  max_value=154.0,
-                                  value=2.0,
-                                  step=1.0)
-Fib = st.sidebar.number_input(label='Fib', min_value=0.8,
-                                  max_value=8.4,
-                                  value=0.8,
-                                  step=0.2)
-ALB = st.sidebar.number_input(label='ALB', min_value=23.3,
-                                  max_value=68.7,
-                                  value=24.0,
-                                  step=1.0)
-AST = st.sidebar.number_input(label='AST', min_value=2.6,
-                                  max_value=333.0,
-                                  value=5.0,
-                                  step=5.0)
+HCT = st.sidebar.number_input(label='HCT', min_value=17.70,
+                                  max_value=58.50,
+                                  value=18.00,
+                                  step=2.20)
+
+Neu = st.sidebar.number_input(label='Neu', min_value=1.00,
+                                  max_value=25.00,
+                                  value=2.00,
+                                  step=1.00)
+PT = st.sidebar.number_input(label='PT', min_value=8.90,
+                                  max_value=154.40,
+                                  value=9.20,
+                                  step=1.00)
+Fib = st.sidebar.number_input(label='Fib', min_value=0.81,
+                                  max_value=8.04,
+                                  value=0.81,
+                                  step=0.02)
+ALB = st.sidebar.number_input(label='ALB', min_value=23.30,
+                                  max_value=68.70,
+                                  value=24.00,
+                                  step=1.00)
+AST = st.sidebar.number_input(label='AST', min_value=0.00,
+                                  max_value=377.10,
+                                  value=5.00,
+                                  step=5.00)
 
 
 
 
-features = {'Age': Age, 'Gender': Gender,
+features = {'Age': Age, 'Gender': Gender,'BMI':BMI,
             'Smokeing': Smokeing, 'Drinking': Drinking,
             'Diabetes': Diabetes, 'Heart.disease': Heart_disease,
             'WBC': WBC, 'RBC': RBC,
